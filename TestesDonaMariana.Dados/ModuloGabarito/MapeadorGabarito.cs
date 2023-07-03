f265cc3d83c71bc39d406e33b4c0197f9f3541bc
Rafael Santos <rafaelsantos138@hotmail.com>
2023-07-03 17:55:40 -0300

    iniciado mapeador gabarito

---
+++ TestesDonaMariana.Dados/ModuloGabarito/MapeadorGabarito.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using TestesDonaMariana.Dominio.ModuloGabarito;
+
+namespace TestesDonaMariana.Dados.ModuloGabarito
+{
+    public class MapeadorGabarito : MapeadorBase<Gabarito>
+    {
+        public override void ConfigurarParametros(SqlCommand comando, Gabarito registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Gabarito ConverterRegistro(SqlDataReader leitorRegistros)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
