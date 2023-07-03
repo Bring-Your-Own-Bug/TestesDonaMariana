428e5eaac087b39e87d83252312c9c6ac96a21c9
Rafael Santos <rafaelsantos138@hotmail.com>
2023-07-03 17:07:15 -0300

    Criado mapeador base

---
+++ TestesDonaMariana.Dados/Compartilhado/MapeadorBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TestesDonaMariana.Dados.Compartilhado
+{
+    public abstract class MapeadorBase<T>
+    {
+        public abstract void ConfigurarParametros(SqlCommand comando, T registro);
+
+        public abstract T ConverterRegistro(SqlDataReader leitorRegistros);
+    }
+}
