39856b8a9895fa924137bcbfbce6c9b7cf07b358
Rafael Santos <rafaelsantos138@hotmail.com>
2023-07-03 17:56:53 -0300

    iniciado mapeador questao

---
+++ TestesDonaMariana.Dados/ModuloQuestao/MapeadorQuestao.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using TestesDonaMariana.Dominio.ModuloQuestao;
+
+namespace TestesDonaMariana.Dados.ModuloQuestao
+{
+    public class MapeadorQuestao : MapeadorBase<Questao>
+    {
+        public override void ConfigurarParametros(SqlCommand comando, Questao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Questao ConverterRegistro(SqlDataReader leitorRegistros)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
