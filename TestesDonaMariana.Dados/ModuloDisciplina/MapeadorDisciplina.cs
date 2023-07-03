7ad1261ba668f8bf2276825eb6e26eeba14ae87c
Rafael Santos <rafaelsantos138@hotmail.com>
2023-07-03 17:54:43 -0300

    iniciado mapeador disciplina

---
+++ TestesDonaMariana.Dados/ModuloDisciplina/MapeadorDisciplina.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using TestesDonaMariana.Dominio.ModuloDisciplina;
+
+namespace TestesDonaMariana.Dados.ModuloDisciplina
+{
+    public class MapeadorDisciplina : MapeadorBase<Disciplina>
+    {
+        public override void ConfigurarParametros(SqlCommand comando, Disciplina registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Disciplina ConverterRegistro(SqlDataReader leitorRegistros)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
