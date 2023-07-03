06cbd37079d0bbec101257fc80172817e8831738
Rafael Santos <rafaelsantos138@hotmail.com>
2023-07-03 17:56:16 -0300

    iniciado mapeador materia

---
+++ TestesDonaMariana.Dados/ModuloMateria/MapeadorMateria.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using TestesDonaMariana.Dominio.ModuloMateria;
+
+namespace TestesDonaMariana.Dados.ModuloMateria
+{
+    public class MapeadorMateria : MapeadorBase<Materia>
+    {
+        public override void ConfigurarParametros(SqlCommand comando, Materia registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Materia ConverterRegistro(SqlDataReader leitorRegistros)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
