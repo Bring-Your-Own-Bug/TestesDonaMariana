984944505d428e9c945898ea34047bde6c40a421
ljoaolucasl <claudinojoaolucas@gmail.com>
2023-07-04 15:16:02 -0300

    Implementando Tela Principal

--- TestesDonaMariana.WinApp/TelaPrincipalForm.Designer.cs
+++ TestesDonaMariana.WinApp/TelaPrincipalForm.Designer.cs
@@ -28,12 +28,268 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            toolStrip1 = new ToolStrip();
+            btnDisciplina = new ToolStripButton();
+            btnMateria = new ToolStripButton();
+            btnQuestao = new ToolStripButton();
+            btnTeste = new ToolStripButton();
+            barraFuncoes = new ToolStrip();
+            btnAdicionar = new ToolStripButton();
+            btnEditar = new ToolStripButton();
+            btnExcluir = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            btnAddItem = new ToolStripButton();
+            btnAttStatus = new ToolStripButton();
+            lbTipoCadastro = new ToolStripLabel();
+            statusStrip1 = new StatusStrip();
+            lbStatus = new ToolStripStatusLabel();
+            plPrincipal = new Panel();
+            toolStrip1.SuspendLayout();
+            barraFuncoes.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.BackColor = Color.FromArgb(0, 165, 100);
+            toolStrip1.GripStyle = ToolStripGripStyle.Hidden;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnDisciplina, btnMateria, btnQuestao, btnTeste });
+            toolStrip1.LayoutStyle = ToolStripLayoutStyle.Flow;
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1013, 58);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // btnDisciplina
+            // 
+            btnDisciplina.BackColor = Color.FromArgb(0, 165, 100);
+            btnDisciplina.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnDisciplina.Font = new Font("Tahoma", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDisciplina.ForeColor = Color.White;
+            btnDisciplina.ImageTransparentColor = Color.Magenta;
+            btnDisciplina.Margin = new Padding(0);
+            btnDisciplina.Name = "btnDisciplina";
+            btnDisciplina.Padding = new Padding(15);
+            btnDisciplina.RightToLeft = RightToLeft.No;
+            btnDisciplina.Size = new Size(129, 58);
+            btnDisciplina.Text = "Disciplina";
+            btnDisciplina.TextDirection = ToolStripTextDirection.Horizontal;
+            btnDisciplina.Click += btnDisciplina_Click;
+            btnDisciplina.MouseEnter += btnColor_MouseEnter;
+            btnDisciplina.MouseLeave += btnColor_MouseLeave;
+            // 
+            // btnMateria
+            // 
+            btnMateria.BackColor = Color.FromArgb(0, 165, 100);
+            btnMateria.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnMateria.Font = new Font("Tahoma", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnMateria.ForeColor = Color.White;
+            btnMateria.ImageTransparentColor = Color.Magenta;
+            btnMateria.Margin = new Padding(0);
+            btnMateria.Name = "btnMateria";
+            btnMateria.Padding = new Padding(15);
+            btnMateria.RightToLeft = RightToLeft.No;
+            btnMateria.Size = new Size(111, 58);
+            btnMateria.Text = "Matéria";
+            btnMateria.TextDirection = ToolStripTextDirection.Horizontal;
+            btnMateria.Click += btnMateria_Click;
+            btnMateria.MouseEnter += btnColor_MouseEnter;
+            btnMateria.MouseLeave += btnColor_MouseLeave;
+            // 
+            // btnQuestao
+            // 
+            btnQuestao.BackColor = Color.FromArgb(0, 165, 100);
+            btnQuestao.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnQuestao.Font = new Font("Tahoma", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnQuestao.ForeColor = Color.White;
+            btnQuestao.ImageTransparentColor = Color.Magenta;
+            btnQuestao.Margin = new Padding(0);
+            btnQuestao.Name = "btnQuestao";
+            btnQuestao.Padding = new Padding(15);
+            btnQuestao.RightToLeft = RightToLeft.No;
+            btnQuestao.Size = new Size(118, 58);
+            btnQuestao.Text = "Questão";
+            btnQuestao.TextDirection = ToolStripTextDirection.Horizontal;
+            btnQuestao.Click += btnQuestao_Click;
+            btnQuestao.MouseEnter += btnColor_MouseEnter;
+            btnQuestao.MouseLeave += btnColor_MouseLeave;
+            // 
+            // btnTeste
+            // 
+            btnTeste.BackColor = Color.FromArgb(0, 165, 100);
+            btnTeste.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnTeste.Font = new Font("Tahoma", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTeste.ForeColor = Color.White;
+            btnTeste.ImageTransparentColor = Color.Magenta;
+            btnTeste.Margin = new Padding(0);
+            btnTeste.Name = "btnTeste";
+            btnTeste.Padding = new Padding(15);
+            btnTeste.RightToLeft = RightToLeft.No;
+            btnTeste.Size = new Size(94, 58);
+            btnTeste.Text = "Teste";
+            btnTeste.TextDirection = ToolStripTextDirection.Horizontal;
+            btnTeste.Click += btnTeste_Click;
+            btnTeste.MouseEnter += btnColor_MouseEnter;
+            btnTeste.MouseLeave += btnColor_MouseLeave;
+            // 
+            // barraFuncoes
+            // 
+            barraFuncoes.BackColor = Color.White;
+            barraFuncoes.GripStyle = ToolStripGripStyle.Hidden;
+            barraFuncoes.Items.AddRange(new ToolStripItem[] { btnAdicionar, btnEditar, btnExcluir, toolStripSeparator1, btnAddItem, btnAttStatus, lbTipoCadastro });
+            barraFuncoes.Location = new Point(0, 58);
+            barraFuncoes.Name = "barraFuncoes";
+            barraFuncoes.Size = new Size(1013, 54);
+            barraFuncoes.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.BackColor = Color.FromArgb(80, 230, 80);
+            btnAdicionar.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAdicionar.Image = TestesDonaMariana.WinApp.Properties.Resources.add;
+            btnAdicionar.ImageScaling = ToolStripItemImageScaling.None;
+            btnAdicionar.ImageTransparentColor = Color.Magenta;
+            btnAdicionar.Margin = new Padding(10);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Padding = new Padding(5);
+            btnAdicionar.Size = new Size(103, 34);
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.Click += btnAdd_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.BackColor = Color.FromArgb(80, 130, 230);
+            btnEditar.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnEditar.Image = TestesDonaMariana.WinApp.Properties.Resources.edit;
+            btnEditar.ImageScaling = ToolStripItemImageScaling.None;
+            btnEditar.ImageTransparentColor = Color.Magenta;
+            btnEditar.Margin = new Padding(10);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Padding = new Padding(5);
+            btnEditar.Size = new Size(79, 34);
+            btnEditar.Text = "Editar";
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.BackColor = Color.FromArgb(230, 80, 80);
+            btnExcluir.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnExcluir.Image = TestesDonaMariana.WinApp.Properties.Resources.remove;
+            btnExcluir.ImageScaling = ToolStripItemImageScaling.None;
+            btnExcluir.ImageTransparentColor = Color.Magenta;
+            btnExcluir.Margin = new Padding(10);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Padding = new Padding(5);
+            btnExcluir.Size = new Size(85, 34);
+            btnExcluir.Text = "Excluir";
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 54);
+            // 
+            // btnAddItem
+            // 
+            btnAddItem.BackColor = Color.Yellow;
+            btnAddItem.Enabled = false;
+            btnAddItem.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAddItem.Image = TestesDonaMariana.WinApp.Properties.Resources.addItem;
+            btnAddItem.ImageScaling = ToolStripItemImageScaling.None;
+            btnAddItem.ImageTransparentColor = Color.Magenta;
+            btnAddItem.Margin = new Padding(10);
+            btnAddItem.Name = "btnAddItem";
+            btnAddItem.Padding = new Padding(5);
+            btnAddItem.Size = new Size(135, 34);
+            btnAddItem.Text = "Adicionar Item";
+            btnAddItem.Click += btnAddItem_Click;
+            // 
+            // btnAttStatus
+            // 
+            btnAttStatus.BackColor = Color.Orange;
+            btnAttStatus.Enabled = false;
+            btnAttStatus.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAttStatus.Image = TestesDonaMariana.WinApp.Properties.Resources.statusCheck;
+            btnAttStatus.ImageScaling = ToolStripItemImageScaling.None;
+            btnAttStatus.ImageTransparentColor = Color.Magenta;
+            btnAttStatus.Margin = new Padding(10);
+            btnAttStatus.Name = "btnAttStatus";
+            btnAttStatus.Padding = new Padding(5);
+            btnAttStatus.Size = new Size(142, 34);
+            btnAttStatus.Text = "Atualizar Status";
+            btnAttStatus.Click += btnAttStatus_Click;
+            // 
+            // lbTipoCadastro
+            // 
+            lbTipoCadastro.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lbTipoCadastro.Name = "lbTipoCadastro";
+            lbTipoCadastro.Size = new Size(0, 51);
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lbStatus });
+            statusStrip1.Location = new Point(0, 604);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1013, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lbStatus
+            // 
+            lbStatus.Name = "lbStatus";
+            lbStatus.Size = new Size(0, 17);
+            // 
+            // plPrincipal
+            // 
+            plPrincipal.Dock = DockStyle.Fill;
+            plPrincipal.Location = new Point(0, 112);
+            plPrincipal.Name = "plPrincipal";
+            plPrincipal.Size = new Size(1013, 492);
+            plPrincipal.TabIndex = 3;
+            plPrincipal.ControlAdded += plPrincipal_ControlAdded;
+            plPrincipal.ControlRemoved += plPrincipal_ControlRemoved;
+            // 
+            // TelaPrincipalForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1013, 626);
+            Controls.Add(plPrincipal);
+            Controls.Add(barraFuncoes);
+            Controls.Add(statusStrip1);
+            Controls.Add(toolStrip1);
+            Name = "TelaPrincipalForm";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Testes Dona Mariana";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            barraFuncoes.ResumeLayout(false);
+            barraFuncoes.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton btnDisciplina;
+        private ToolStripButton btnMateria;
+        private ToolStripButton btnQuestao;
+        private ToolStripButton btnTeste;
+        private ToolStrip barraFuncoes;
+        private ToolStripButton btnAdicionar;
+        private ToolStripButton btnEditar;
+        private ToolStripButton btnExcluir;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lbStatus;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripLabel lbTipoCadastro;
+        private Panel plPrincipal;
+        private ToolStripButton btnAddItem;
+        private ToolStripButton btnAttStatus;
     }
 }
--- TestesDonaMariana.WinApp/TelaPrincipalForm.cs
+++ TestesDonaMariana.WinApp/TelaPrincipalForm.cs
@@ -1,20 +1,218 @@
 using TestesDonaMariana.Dados.ModuloDisciplina;
-using TestesDonaMariana.Dominio.Compartilhado;
-using TestesDonaMariana.Dominio.ModuloDisciplina;
+using TestesDonaMariana.Dados.ModuloGabarito;
+using TestesDonaMariana.Dados.ModuloMateria;
+using TestesDonaMariana.Dados.ModuloQuestao;
+using TestesDonaMariana.Dados.ModuloTeste;
 using TestesDonaMariana.WinApp.ModuloDisciplina;
+using TestesDonaMariana.WinApp.ModuloGabarito;
+using TestesDonaMariana.WinApp.ModuloMateria;
+using TestesDonaMariana.WinApp.ModuloQuestao;
+using TestesDonaMariana.WinApp.ModuloTeste;
 
 namespace TestesDonaMariana.WinApp
 {
     public partial class TelaPrincipalForm : Form
     {
+        private Dictionary<Control, ToolStripButton> coresBotoes = new();
+
+        private IControladorBase _controladorBase;
+        private UserControl _tabela;
+
+        private static TelaPrincipalForm _telaPrincipal;
+
+        private RepositorioDisciplina _repositorioDisciplina = new();
+        private RepositorioMateria _repositorioMateria = new();
+        private RepositorioQuestao _repositorioQuestao = new();
+        private RepositorioTeste _repositorioTeste = new();
+        private RepositorioGabarito _repositorioGabarito = new();
+
+        private TabelaDisciplinaControl _tabelaDisciplina = new();
+        private TabelaMateriaControl _tabelaMateria = new();
+        private TabelaQuestaoControl _tabelaQuestao = new();
+        private TabelaTesteControl _tabelaTeste = new();
+        private TabelaGabaritoControl _tabelaGabarito = new();
+
         public TelaPrincipalForm()
         {
             InitializeComponent();
+
+            _telaPrincipal = this;
+
+            AdicionarBotoesDicionario();
+        }
+
+        private void AdicionarBotoesDicionario()
+        {
+            coresBotoes.Add(_tabelaMateria, btnDisciplina);
+            coresBotoes.Add(_tabelaQuestao, btnMateria);
+            coresBotoes.Add(_tabelaTeste, btnQuestao);
+            coresBotoes.Add(_tabelaDisciplina, btnTeste);
         }
 
         public static void AtualizarStatus(string status)
         {
-            //_telaPrincipal.lbStatus.Text = status;
+            _telaPrincipal.lbStatus.Text = status;
+        }
+
+        private void btnDisciplina_Click(object sender, EventArgs e)
+        {
+            _controladorBase = new ControladorDisciplina(_repositorioDisciplina, _tabelaDisciplina, _repositorioMateria);
+            ConfigurarTelaPrincipal();
+        }
+
+        private void btnMateria_Click(object sender, EventArgs e)
+        {
+            _controladorBase = new ControladorMateria(_repositorioMateria, _tabelaMateria, _repositorioDisciplina, _repositorioQuestao);
+
+            ConfigurarTelaPrincipal();
+        }
+
+        private void btnQuestao_Click(object sender, EventArgs e)
+        {
+            _controladorBase = new ControladorQuestao(_repositorioQuestao, _tabelaQuestao, _repositorioMateria);
+
+            ConfigurarTelaPrincipal();
+        }
+
+        private void btnTeste_Click(object sender, EventArgs e)
+        {
+            _controladorBase = new ControladorTeste(_repositorioTeste, _tabelaTeste, _repositorioDisciplina, _repositorioQuestao);
+
+            ConfigurarTelaPrincipal();
+        }
+
+        private void ConfigurarTelaPrincipal()
+        {
+            ConfigurarListagem();
+
+            ConfigurarToolTipsAndButtons();
+
+            AbrirListagem();
+
+            ResetarBotoes();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _controladorBase.Adicionar();
+            ResetarBotoes();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            _controladorBase.Editar();
+            ResetarBotoes();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            _controladorBase.Excluir();
+            ResetarBotoes();
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            _controladorBase.AdicionarItens();
+            ResetarBotoes();
+        }
+
+        private void btnAttStatus_Click(object sender, EventArgs e)
+        {
+            _controladorBase.AtualizarStatus();
+            ResetarBotoes();
+        }
+
+        private void AbrirListagem()
+        {
+            lbTipoCadastro.Text = _controladorBase.ObterTipoCadastro();
+            plPrincipal.Controls.Clear();
+            plPrincipal.Controls.Add(_tabela);
+            _controladorBase.CarregarRegistros();
+        }
+
+        private void ConfigurarListagem()
+        {
+            _tabela = _controladorBase.ObterListagem();
+
+            _tabela.Dock = DockStyle.Fill;
+        }
+
+        private void ConfigurarToolTipsAndButtons()
+        {
+            btnAdicionar.ToolTipText = _controladorBase.ToolTipAdicionar;
+            btnEditar.ToolTipText = _controladorBase.ToolTipEditar;
+            btnExcluir.ToolTipText = _controladorBase.ToolTipExcluir;
+            barraFuncoes.Visible = true;
+        }
+
+        private void ResetarBotoes()
+        {
+            //ConfigurarBotaoItem();
+
+            //ConfigurarBotaoAtualizarStatus();
+
+            if (((DataGridView)_tabela.Controls[0]).SelectedRows.Count > 0)
+            {
+                btnEditar.Enabled = true;
+                btnExcluir.Enabled = true;
+            }
+            else
+            {
+                btnEditar.Enabled = false;
+                btnExcluir.Enabled = false;
+            }
+        }
+
+        //private void ConfigurarBotaoItem()
+        //{
+        //    if (((DataGridView)_tabela.Controls[0]).SelectedRows.Count > 0 && _controladorBase is ControladorQuestao)
+        //        btnAddItem.Enabled = true;
+        //    else
+        //        btnAddItem.Enabled = false;
+        //}
+
+        //private void ConfigurarBotaoAtualizarStatus()
+        //{
+        //    if (((DataGridView)_tabela.Controls[0]).SelectedRows.Count > 0 && _controladorBase is ControladorAluguel)
+        //        btnAttStatus.Enabled = true;
+        //    else
+        //        btnAttStatus.Enabled = false;
+        //}
+
+        private void btnColor_MouseEnter(object sender, EventArgs e)
+        {
+            ToolStripButton btn = (ToolStripButton)sender;
+
+            btn.ForeColor = Color.Black;
+        }
+
+        private void btnColor_MouseLeave(object sender, EventArgs e)
+        {
+            ToolStripButton btn = (ToolStripButton)sender;
+
+            btn.ForeColor = Color.White;
+        }
+
+        private void plPrincipal_ControlAdded(object sender, ControlEventArgs e)
+        {
+            ToolStripButton btn;
+
+            coresBotoes.TryGetValue(e.Control, out btn);
+
+            btn.BackColor = Color.White;
+            btn.ForeColor = Color.Black;
+            btn.MouseLeave -= btnColor_MouseLeave;
+        }
+
+        private void plPrincipal_ControlRemoved(object sender, ControlEventArgs e)
+        {
+            ToolStripButton btn;
+
+            coresBotoes.TryGetValue(e.Control, out btn);
+
+            btn.BackColor = Color.FromArgb(0, 165, 100);
+            btn.ForeColor = Color.White;
+            btn.MouseLeave += btnColor_MouseLeave;
         }
     }
 }
