1e4f477c6bb08424dc97a21c81048ab905f65b71
Rafael Santos <rafaelsantos138@hotmail.com>
2023-07-05 16:03:33 -0300

    repositorio teste iniciado

--- TestesDonaMariana.Dados/ModuloTeste/RepositorioTeste.cs
+++ TestesDonaMariana.Dados/ModuloTeste/RepositorioTeste.cs
@@ -4,7 +4,25 @@ namespace TestesDonaMariana.Dados.ModuloTeste
 {
     public class RepositorioTeste : RepositorioBaseSql<Teste>
     {
-        protected override string AddCommand => throw new NotImplementedException();
+        protected override string AddCommand => @"INSERT INTO [dbo].[TBTESTE]
+                                                           (
+                                                                [TITULO]
+                                                               ,[NUMERODEQUESTOES]
+                                                               ,[DISCIPLINA_ID]
+                                                               ,[MATERIA_ID]
+                                                               ,[LISTA_QUESTAO_ID]
+                                                               ,[DATAGERACAO_ID]
+                                                           )
+                                                     VALUES
+                                                           (
+                                                                 @TITULO
+                                                                ,@NUMERODEQUESTOES
+                                                                ,@DISCIPLINA_ID
+                                                                ,@MATERIA_ID
+                                                                ,@LISTA_QUESTAO_ID
+                                                                ,@DATAGERACAO_ID
+                                                           )
+                                                           SELECT SCOPE_IDENTITY();";
 
         protected override string EditCommand => throw new NotImplementedException();
 
