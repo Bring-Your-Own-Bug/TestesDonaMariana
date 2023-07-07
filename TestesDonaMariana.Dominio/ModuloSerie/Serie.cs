1925f5b10b0eaabe65d90ee105d1100bcd84a935
Rafael Santos <rafaelsantos138@hotmail.com>
2023-07-07 13:21:44 -0300

    Implementado descrição ao enum Serie

--- TestesDonaMariana.Dominio/ModuloSerie/Serie.cs
+++ TestesDonaMariana.Dominio/ModuloSerie/Serie.cs
@@ -1,8 +1,12 @@
-﻿namespace TestesDonaMariana.Dominio.ModuloSerie
+﻿using System.ComponentModel;
+
+namespace TestesDonaMariana.Dominio.ModuloSerie
 {
     public enum Serie
     {
+        [Description("1ª Série")]
         Primeira,
+        [Description("2ª Série")]
         Segunda
     }
 }
